e0b8f7d95cd31e02644ebf4f4710c039cf3b5b68
Greg Baranov <gbaranov99@gmail.com>
2019-10-21 17:53:27 +0000

    Template now has https support, but F on mozilla observatory

--- frontend/src/store/services/api.js
+++ frontend/src/store/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export default axios.create({
-  baseURL: 'http://www.totrno.com/api/',
+  baseURL: 'https://www.totrno.com/api/',
   timeout: 5000,
   headers: {
     'Content-Type': 'application/json',
